7eede7c710f2d944f73c7ab4e35004568d8e5a7e
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-08-22 15:46:42 +0900

    update

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 CORS(app)
 app.config["JSON_AS_ASCII"] = False
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
 #アカウント連携ページ
 # 受け取ったjsonデータからプレイリストを取得
 # データベース登録関数へデータを渡す
@@ -19,28 +15,24 @@ def account_align():
     data = request.get_json()
     # キーを取得
     keys = data["spotify_data"].keys()
-    
     playlist_count = len(data["spotify_data"])
-    
 
     songAndArtist=[]
 
     for i in keys:
-
         playlists = data["spotify_data"][i]
         for playlist in playlists:
             song=playlist["song"]
             artist=playlist["artist"]
-            
-            songAndArtist.append([song,artist])
 
-    print(songAndArtist)     
+            songAndArtist.append([song,artist])
 
     spotify_data = songAndArtist
     display_name = data.get("display_name")
     user_id = data.get("user_id")
+
+    print(songAndArtist)
     print(spotify_data,display_name,user_id)
-   
     return database.regist_data(spotify_data,display_name,user_id)
 
 #合言葉を打ってグループを作成・参加確認
